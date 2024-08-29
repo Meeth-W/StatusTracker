54ac431cd32a7416b90cf791c50ca91254f3f598
Meeth <meeth2111@gmail.com>
2024-08-29 17:27:58 +0530

    ✅ Added Logging

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import random
+import logging
+import platform
 
 import discord
 from discord.ext import commands, tasks
@@ -11,11 +13,57 @@ from dotenv import load_dotenv
 from functions import ConfigManager
 config = ConfigManager('config.yml')
 
-load_dotenv()
-
 # Intents 
 intents = discord.Intents.default()
 intents.message_content = True
+
+# Logging Handlers
+class LoggingFormatter(logging.Formatter):
+    # Colors
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(reset)", self.reset)
+        format = format.replace("(levelcolor)", log_color)
+        format = format.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+logger: logging.Logger  = logging.getLogger('discord_bot')
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 # Main Bot
 class Tracker(commands.Bot):
@@ -23,9 +71,11 @@ class Tracker(commands.Bot):
         super().__init__(
             command_prefix = commands.when_mentioned_or(config.fetch('prefix')),
             intents = intents,
-            help_command = None
+            # help_command = None
         )
         self.api_key = os.getenv('APIKEY')
+        self.logger = logger
+        self.database = None
     
     async def load_cogs(self) -> None:
         """
@@ -36,10 +86,10 @@ class Tracker(commands.Bot):
                 extension = file[:-3]
                 try:
                     await self.load_extension(f"modules.{extension}")
-                    print(f"Loaded extension '{extension}'")
+                    self.logger.info(f"Loaded extension '{extension}'")
                 except Exception as e:
                     exception = f"{type(e).__name__}: {e}"
-                    print(f"Failed to load extension {extension}\n{exception}")
+                    self.logger.error(f"Failed to load extension {extension}\n{exception}")
                 
     @tasks.loop(minutes=1.0)
     async def status_task(self) -> None:
@@ -54,6 +104,13 @@ class Tracker(commands.Bot):
         await self.wait_until_ready()
     
     async def setup_hook(self) -> None:
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
         await self.load_cogs()
         self.status_task.start()
     
@@ -61,6 +118,8 @@ class Tracker(commands.Bot):
         if message.author == self.user or message.author.bot:
             return
         await self.process_commands(message)
+
+load_dotenv()
 
 bot = Tracker()
 bot.run(os.getenv("TOKEN"))
